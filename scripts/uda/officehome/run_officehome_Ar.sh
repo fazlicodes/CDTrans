17273f96956531869c30131c67c963a35de39480
ext_group7 <ext_group7@p3-r56-b.g42cloud.net>
2023-02-10 20:15:27 +0400

    update scripts

--- scripts/uda/officehome/run_officehome_Ar.sh
+++ scripts/uda/officehome/run_officehome_Ar.sh
@@ -15,9 +15,9 @@ else
 fi
 for target_dataset in 'Product' 'Real_World' 'Clipart' # 
 do
-    python train.py --config_file configs/uda.yml MODEL.DEVICE_ID $gpus \
+    python test.py --config_file configs/uda.yml MODEL.DEVICE_ID $gpus \
     OUTPUT_DIR '../logs/uda/'$model'/office-home/Art2'$target_dataset \
-    MODEL.PRETRAIN_PATH '../logs/pretrain/'$model'/office-home/Art/transformer_10.pth' \
+    MODEL.PRETRAIN_PATH '../logs/trans4DA/transformer_120.pth' \
     DATASETS.ROOT_TRAIN_DIR './data/OfficeHomeDataset/Art.txt' \
     DATASETS.ROOT_TRAIN_DIR2 './data/OfficeHomeDataset/'$target_dataset'.txt' \
     DATASETS.ROOT_TEST_DIR './data/OfficeHomeDataset/'$target_dataset'.txt' \
