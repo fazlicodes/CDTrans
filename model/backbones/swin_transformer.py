ba77a2660d62b03fe74468a945998e0aa228ab73
Rufael Marew <rufaelfekadu48@gmail.com>
2023-02-25 02:22:33 +0400

    minor update on swin transformer

--- model/backbones/swin_transformer.py
+++ model/backbones/swin_transformer.py
@@ -604,7 +604,7 @@ class SwinTransformer(nn.Module):
         return x
 
     def forward(self, x, cam_label=None, view_label=None):
-        x = self.forward_features(x)
+        x = self.forward_features(x, cam_label, view_label)
         x = self.head(x)
         return x
 
