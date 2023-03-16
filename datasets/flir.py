920e97609b3f94cf50020128d2e80b583b99ecdf
Rufael Marew <rufaelfekadu48@gmail.com>
2023-03-17 01:59:01 +0400

    target only

--- datasets/flir.py
+++ datasets/flir.py
@@ -96,14 +96,14 @@ def build_dataset(cfg):
     ])
     nb_classes=3
     num_workers = cfg.DATALOADER.NUM_WORKERS
-    train_dataset = Coco(label_path='/project/CDTrans/data/train_labels.txt',
-                root_dir='/project/CDTrans/data/cocoflir/', transform=train_transforms)
+    train_dataset = Coco(label_path='/nfs/users/ext_group7/project/CDTrans/project/CDTrans/data/train_labels.txt',
+                root_dir='/nfs/users/ext_group7/project/CDTrans/project/CDTrans/data/cocoflir/', transform=train_transforms)
     
-    train_set_normal = Coco(label_path='/project/CDTrans/data/train_labels.txt',
-                root_dir='/project/CDTrans/data/cocoflir/', transform=val_transforms)
+    train_set_normal = Coco(label_path='/nfs/users/ext_group7/project/CDTrans/project/CDTrans/data/train_labels.txt',
+                root_dir='/nfs/users/ext_group7/project/CDTrans/project/CDTrans/data/cocoflir/', transform=val_transforms)
     
-    val_dataset = Coco(label_path='/project/CDTrans/data/val_labels.txt',
-                root_dir='/project/CDTrans/data/cocoflir/', transform=val_transforms)
+    val_dataset = Coco(label_path='/nfs/users/ext_group7/project/CDTrans/project/CDTrans/data/val_labels.txt',
+                root_dir='/nfs/users/ext_group7/project/CDTrans/project/CDTrans/data/cocoflir/', transform=val_transforms)
             
     train_loader = DataLoader(
         train_set, batch_size=cfg.SOLVER.IMS_PER_BATCH, shuffle=True, num_workers=num_workers,
